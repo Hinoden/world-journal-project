9ee65736a9a5328786fb98104558a83f4228c561
Hinoden <krwong3@gmail.com>
2026-01-30 00:55:26 -0800

    mongodb connected

---
+++ server/models/Entry.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const entrySchema = new mongoose.Schema({
+    title: {type: String, required: true,},
+    emotion: {type: String, required: true,},
+    content: {type: String, required: true,},
+    date: {type: Date, default: Date.now,},
+});
+
+const entryModel = mongoose.model('Entry', entrySchema);
+
+export default Entry;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URI);
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
