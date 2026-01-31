a906ea187648cab9d92722a291bffabc84802be7
Hinoden <krwong3@gmail.com>
2026-01-31 15:13:04 -0800

    preparing to deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import entryRoutes from "../routes/entries.js";
+import entryRoutes from "./routes/entries.js";
 
 const app = express();
 
