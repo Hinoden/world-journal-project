e35c112e8e0d98ecb726dbfb3899115321bdb0ae
Hinoden <krwong3@gmail.com>
2026-01-31 01:27:02 -0800

    Added github link + navbar logo

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -11,7 +11,10 @@ function About() {
             <h1 className="about-title">The World Journal Project</h1>
             <p className="about-subtitle">Daily updates. Real people. Around the world.</p>
             <p className="about-text">I've always been fascinated with the way our lives differ from one another despite experiencing the very same day. Today, someone might be receiving their dream college acceptance letter, while another is scrambling around at work just trying to get through another hard day. Somewhere else, someone may be starting a new chapter, saying goodbye to a loved one, celebrating a milestone, or simply finding small moments of peace in the ordinary.</p>
-            <p className="about-text">This project aims to capture these different experiences and share them with the world. Feel free to submit your own stories to add to our collection. I hope in exploring this site, you'll find comfort in the diversity of human experience, and a reminder that no matter where you are in life, you're not alone.</p>
+            <p className="about-text">This project aims to capture these different experiences and share them with the world. I hope in exploring this site, you'll find comfort in the diversity of human experience, and a reminder that no matter where you are in life, you're not alone.</p>
+            <p className="about-text">Feel free to submit your own stories to add to our collection. All we ask is for you to be kind and respectful in your submissions. Any entries perpetrating harm will be subject for removal.</p>
+            <p className="about-text">Meet the creator · <a href="https://github.com/hinoden" target="_blank" rel="noopener noreferrer" className="about-link">GitHub</a></p>
+
         </div>
         <Footer />
     </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import '../styles/Navbar.css';
 function Navbar() {
     return (
         <nav>
+            <div className="logo">
+                <Link to="/" className="navLinkLogo"><strong>World Journal Project</strong></Link>
+            </div>
             <div className="links">
                 <Link to="/" className="navLink">Home</Link>
                 <Link to="/about" className="navLink">About</Link>
